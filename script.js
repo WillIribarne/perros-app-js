39c4b0fdc452bb40ef35106777e07b0362799df5
Your Name <iriguille0504@gmail.com>
2026-07-14 10:24:07 -0300

    D1-eventListener agregado y overhauleado

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ const estado = {
 }
 
 const contenedor = document.getElementById('contenedor-resultados')
+const botonBuscar = document.getElementById("botonBuscar")
+const inputRaza = document.getElementById("inputRaza")
 
-const render = (estado) => estado.fotos.forEach(foto => {
-    const img = document.createElement('img');
-    img.src = foto;
-    contenedor.appendChild(img);
-});
-
-const limpiarPerritos = () => contenedor.innerHTML = ''
+const render = () => {
+    limpiarPerritos();
+    buscarPerritos(inputRaza.value);
+        estado.fotos.forEach(urlFoto => {
+        const img = document.createElement('img');
+        img.src = urlFoto;
+        contenedor.appendChild(img);
+    });
+}
 
 const buscarPerritos = (raza) => {
     estado.raza = raza; // Guardamos el texto directo en el estado
@@ -21,6 +25,11 @@ const buscarPerritos = (raza) => {
         'https://images.dog.ceo/breeds/retriever-golden/n02099601_4312.jpg',
         'https://images.dog.ceo/breeds/retriever-golden/n02099601_4651.jpg'
     ];
-    
-    render(estado);
 }
+
+const limpiarPerritos = () => {
+    contenedor.innerHTML = ''
+    estado.fotos.splice(0) //elimina todas las fotos - es como asignar [] pero con mayor lentitud y sin descuidar la memoria (imaginando que js no tiene un garbagecollector...)
+}
+
+botonBuscar.addEventListener("click", render)
