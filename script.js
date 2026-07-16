4f0faccc77b18190fddb6bf99c4e36d40f7c0b19
Your Name <iriguille0504@gmail.com>
2026-07-16 09:54:34 -0300

    D2- muestra mensaje error, visuales mejoradas

--- script.js
+++ script.js
@@ -8,33 +8,38 @@ const botonBuscar = document.getElementById("botonBuscar")
 const inputRaza = document.getElementById("inputRaza")
 
 const render = async () => {
-    contenedor.innerHTML = ''
     await buscarPerritos(inputRaza.value);
         estado.fotos.forEach(urlFoto => {
         const img = document.createElement('img');
         img.src = urlFoto;
+        img.className = 'fotoPerro'
         contenedor.appendChild(img);
     });
 }
 
 async function buscarPerritos(raza) {
-  const url = `https://dog.ceo/api/breed/${raza.toLowerCase().trim()}/images/random`;
+  limpiarContenedor();
+  estado.fotos.splice(0) //elimina todas las fotos - es como asignar [] pero con mayor lentitud y sin descuidar la memoria (imaginando que js no tiene un garbagecollector...)
+  const url = `https://dog.ceo/api/breed/${raza.toLowerCase().trim()}/images/random/8`;
 
   try {
     const respuesta = await fetch(url);
     const datos = await respuesta.json(); // no se va a ejecutar esta linea hasta que haya terminado la anterior
     
     const urlFoto = datos.message;
-    estado.fotos.push(urlFoto);
+    estado.fotos.push(...urlFoto);
     
   } catch (error) {
-    console.error("No se pudo obtener la foto");
+    console.error("No se han podido obtener fotos");
+    const err = document.createElement('p');
+    err.className = 'mensajeError';
+    err.textContent = 'No se han podido obtener fotos';
+    contenedor.appendChild(err);
   }
 }
 
-const limpiarPerritos = () => {
+const limpiarContenedor = () => {
     contenedor.innerHTML = ''
-    estado.fotos.splice(0) //elimina todas las fotos - es como asignar [] pero con mayor lentitud y sin descuidar la memoria (imaginando que js no tiene un garbagecollector...)
 }
 
 botonBuscar.addEventListener("click", render)
