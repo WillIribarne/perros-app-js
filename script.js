54c2fb01f0ac7538c06a20852a91d928dddcd52b
Your Name <iriguille0504@gmail.com>
2026-07-16 11:54:55 -0300

    clase-02

--- script.js
+++ script.js
@@ -40,7 +40,38 @@ function buscarPerritos(raza) {
   });
   return promesaBusqueda;
 };
+/*
+const render = async () => {
+    await buscarPerritos(inputRaza.value)
+        estado.fotos.forEach(urlFoto => {
+        const img = document.createElement('img')
+        img.src = urlFoto
+        img.className = 'fotoPerro'
+        contenedor.appendChild(img)
+    });
+}
 
+async function buscarPerritos(raza) {
+  limpiarContenedor();
+  estado.fotos.splice(0) //elimina todas las fotos - es como asignar [] pero con mayor lentitud y sin descuidar la memoria (imaginando que js no tiene un garbagecollector...)
+  const url = `https://dog.ceo/api/breed/${raza.toLowerCase().trim()}/images/random/8`
+
+  try {
+    const respuesta = await fetch(url)
+    const datos = await respuesta.json() // no se va a ejecutar esta linea hasta que haya terminado la anterior
+    
+    const urlFoto = datos.message
+    estado.fotos.push(...urlFoto)
+    
+  } catch (error) {
+    console.error("No se han podido obtener fotos")
+    const err = document.createElement('p')
+    err.className = 'mensajeError'
+    err.textContent = 'No se han podido obtener fotos'
+    contenedor.appendChild(err)
+  }
+}
+*/
 
 const limpiarContenedor = () => {
     contenedor.innerHTML = ''
